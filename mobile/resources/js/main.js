d22189ffd490ba2cb6f13d309dec0b3a74f5628f
yang-eunjin <jinny920311@gmail.com>
2021-09-19 21:27:54 +0900

    1차완료

--- mobile/resources/js/main.js
+++ mobile/resources/js/main.js
@@ -8,10 +8,6 @@ $(function() {
   var mainSlide = function(){
     var $slick_ele1 = $('.main-visual-content');
 
-    // $slick_ele1.on('init', function(slick){
-    //   _bgMotion(0);
-    // });
-
     $slick_ele1.slick({
       infinite: true,
       autoplay: true,
@@ -19,23 +15,8 @@ $(function() {
       speed: 2000,
       fade:true,
       dots: true,
-      focusOnSelect: false
+      pauseOnFocus: false
     });
-
-
-
-    // $slick_ele1.on('beforeChange', function(event, slick, currentSlide, nextSlide){
-    //   _bgMotion(nextSlide);
-    // });
-    //
-    // function _bgMotion(num){
-    //   var $nextLi = $slick_ele1.find('.slick-slide').eq(num);
-    //   TweenMax.set($('.main-text-img'), {autoAlpha:0, y:80});
-    //   TweenMax.set($('.main-label-img'), {autoAlpha:0, y:80});
-    //
-    //   TweenMax.to($nextLi.find('.main-text-img'), 1, {delay:.3, autoAlpha:1, y:0, ease:Power2.easeOut});
-    //   TweenMax.to($nextLi.find('.main-label-img'), 1, {delay:.6, autoAlpha:1, y:0, ease:Power2.easeOut});
-    // }
   }
 
 
