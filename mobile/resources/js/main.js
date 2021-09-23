fb94b763fcac0fb7c6e78d46f92a3b8292a14755
yang-eunjin <jinny920311@gmail.com>
2021-09-23 18:30:55 +0900

    수정

--- mobile/resources/js/main.js
+++ mobile/resources/js/main.js
@@ -8,6 +8,12 @@ $(function() {
   var mainSlide = function(){
     var $slick_ele1 = $('.main-visual-content');
 
+    $slick_ele1.on('init', function(event, slick){
+      setTimeout(function() {
+        $('.main-visual-content .slick-slide').eq(0).addClass('zoom-out')
+      }, 100)
+    });
+
     $slick_ele1.slick({
       infinite: true,
       autoplay: true,
@@ -15,6 +21,11 @@ $(function() {
       fade:true,
       dots: true,
       pauseOnFocus: false
+    });
+
+    $slick_ele1.on('beforeChange', function (event, slick, currentSlide, nextSlide) {
+      $('.main-visual-content .slick-slide').removeClass('zoom-out')
+      $('.main-visual-content .slick-slide').eq(nextSlide).addClass('zoom-out')
     });
   }
 
